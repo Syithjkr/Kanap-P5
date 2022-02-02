a963bf5852bae82231341470dc6668dde4d1732d
Sandra <c.bollesandra@gmail.com>
2022-02-02 08:26:18 +0100

    création de la variable articles

--- Soutenance/front/Js/script.js
+++ Soutenance/front/Js/script.js
@@ -1,2 +1,10 @@
 /*J'initialise la variable article qui contiendra mon api */
 let articles = [];
+
+/*Je crée une fonction qui récupére tout les articles de mon API */
+async function fetchApi(){
+    await fetch("https://api-kanap-eu.herokuapp.com/api/products")
+      .then((res) => res.json())
+      // Je stock le tout dans une variable articles
+      .then((data) => (artcile = data));
+}
