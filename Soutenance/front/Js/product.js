c3889b2bb00472e9ccd129c1bc1ebb72125b328e
Sandra <c.bollesandra@gmail.com>
2022-02-02 09:08:46 +0100

    recupération de l'id produit

--- Soutenance/front/Js/product.js
+++ Soutenance/front/Js/product.js
@@ -1,1 +1,19 @@
-o
+// on récupére l'id de notre produit depuis l'url pour cibler notre canapé dans l'API
+let productId = window.location.search.replace("?id=", "");
+let image = document.querySelector(".item__img");
+let prix = document.getElementById("price");
+let description = document.getElementById("description");
+let colorSelector = document.getElementById("colors");
+let quantitySelector = document.getElementById("quantity");
+let validateInput = document.getElementById("addToCart");
+quantitySelector.value = "";
+let product = [];
+let cartUser = {
+  name: "",
+  price: "",
+  id: "",
+  color: "",
+  quantity: "0",
+  srcImg: "",
+  altTxt: "",
+};
