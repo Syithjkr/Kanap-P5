3dee46317e01a398f7e2e1763ff0aa68a2a505d6
Sandra <c.bollesandra@gmail.com>
2022-02-02 23:46:18 +0100

    execution

--- Soutenance/front/Js/cart.js
+++ Soutenance/front/Js/cart.js
@@ -11,3 +11,9 @@ contact = {
 products = [];
 orderId = undefined;
 inputError = 0;
+
+// SINON SI nous nous trouvons dans la page panier on execute notre code
+if (location.href.search("confirmation") > 0) {
+    // SINON c'est que nous sommes sur la page "confirmation.html" donc on affiche le numero de commande stocké dans l'URL
+    // et on supprime le panier du localStorage pour pouvoir passer d'autres commandes
+  
